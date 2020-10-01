c07957f456104d80b955e99ba9f66b76540a34e4
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2020-10-01 10:49:05 +0000

    Finish

--- bookers1-debug/db/schema.rb
+++ bookers1-debug/db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_051828) do
+ActiveRecord::Schema.define(version: 2020_10_01_094929) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   create_table "homes", force: :cascade do |t|
