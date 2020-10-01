30b222a892601a51629711ad01d751431836e572
EC2 Default User <ec2-user@ip-172-31-41-123.ec2.internal>
2020-10-01 09:23:50 +0000

    Fix

--- bookers1-debug/db/schema.rb
+++ bookers1-debug/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_060701) do
+ActiveRecord::Schema.define(version: 2020_10_01_085239) do
 
   create_table "books", force: :cascade do |t|
     t.text "body"
